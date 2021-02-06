acc617d9644ec781e26670758fe927c09320d709
Pete McGarty <pmcgarty@hotmail.com>
2021-02-06 13:00:45 -0500

    About Us Component Created - Routers & Params

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,6 +4,7 @@ import CampsiteInfo from './CampsiteInfoComponent';
 import Header from './HeaderComponent';
 import Footer from  './FooterComponent';
 import Home from './HomeComponent';
+import About from './AboutComponent';
 import Contact from  './ContactComponent';
 import { COMMENTS } from '../shared/comments';
 import { PARTNERS } from '../shared/partners';
@@ -40,7 +41,7 @@ class Main extends Component {
             return (
                 <CampsiteInfo
                     campsite={this.state.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
-                    comments={this.state.campsites.filter(comment => comment.campsiteId === +match.params.campsiteId)}
+                    comments={this.state.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
                 />
             )
         }
@@ -50,6 +51,7 @@ class Main extends Component {
                 <Header />
                 <Switch>
                     <Route path='/home' component={HomePage} />
+                    <Route path='/aboutus' render={() => <About partners={this.state.partners} />} />
                     <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} /> } />
                     <Route path='/directory/:campsiteId' component={CampsiteWithId} />
                     <Route exact path='/contactus' component={Contact} />
